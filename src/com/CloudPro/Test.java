df0f9df7921d879e27713f39dc67c225068c33e1
Zeeshan ali <zeeshanali9580@gmail.com>
2020-10-27 02:38:03 +0530

    First Ever commit

---
+++ src/com/CloudPro/Test.java
@@ -0,0 +1,16 @@
+package com.CloudPro;
+
+public class Test {
+
+	public static void main(String[] args) 
+	{
+      String Dev ="y";
+    
+      if(Dev=="y")
+    	  System.out.println("Develop the tas");
+      else
+    	  System.out.println("Test the Code");
+      
+	}
+
+}
